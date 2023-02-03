fe19a6d7274293edbfbac8c9f41d6bea74f38561
iraj norouzi <i.norouzi@mail.cloud-infra.ir>
2023-02-04 02:22:53 +0330

    deleted some unused line

--- backend.tf
+++ backend.tf
@@ -2,8 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-iraj"
     key    = "terraform/terraform.tfstate"
-    # key            = "jhooq/terraform/remote/s3/terraform.tfstate"
     region = "us-east-1"
-    # dynamodb_table = "dynamodb-state-locking"
   }
 }
