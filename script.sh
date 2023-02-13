f57685a0215926c8acc7e6aebff64ed244f1f19e
iraj norouzi <i.norouzi@mail.cloud-infra.ir>
2023-02-13 12:42:36 +0330

    Added Metadata in bash script file

--- script.sh
+++ script.sh
@@ -6,3 +6,4 @@ sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html 
+sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/metadata.html
