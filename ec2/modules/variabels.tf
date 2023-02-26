946267a3bb4f57f962e484792da92aca883dd2e6
iraj norouzi <i.norouzi@mail.cloud-infra.ir>
2023-02-26 12:57:07 +0330

    Clean up

--- ec2/modules/variabels.tf
+++
@@ -1,8 +0,0 @@
-
-variable "AMIS" {}
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
-variable "PUBLIC_KEY" {}
-variable "INSTANCE_USERNAME" {}
-variable "PATH_TO_PRIVATE_KEY" {}
-variable "EC2_DEPLOYMENTS" {}
--- ec2/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Use this for your user data (script from top to bottom)
-# install httpd (Linux 2 version)
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html 
-sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/metadata.html
